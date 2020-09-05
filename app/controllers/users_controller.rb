f2e20cd990d2e30e7e289baaca33fda223215ff1
Mark A Guza <guzamaguza@gmail.com>
2020-09-05 23:15:34 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,20 @@
 class UsersController < ApplicationController
 
-    def new 
-    end 
+    def new
+    end
 
     def create
         @user = User.create(user_params)
         if @user && @user.password == @user.password_confirmation
             session[:user_id] = @user.id
-            redirect_to '/welcome' 
-        else 
+            redirect_to '/welcome'
+        else
             redirect_to '/users/new'
-        end 
+        end
     end
-       
+
     private
-       
+
     def user_params
         params.require(:user).permit(:name, :password, :password_confirmation)
     end
